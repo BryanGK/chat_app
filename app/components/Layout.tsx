373bf9f3eed759740757bd80da5a1ada64fbb18a
Bryan Krauss <bryan@button.is>
2022-05-26 19:19:32 -0700

    refactor: adds layout component

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,3 +1,12 @@
+import { Navbar, MessagesTable, UsersTable } from '../components';
+
 export default function Layout() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h1>Chat App</h1>
+      <UsersTable></UsersTable>
+      <MessagesTable></MessagesTable>
+    </div>
+  );
 }
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,12 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import { Layout } from "../components";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Chat App</title>
         <meta name="description" content="Chat app by BryanGK" />
