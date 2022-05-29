771ba09e5331b1ea8ef6c096e0c310b8ffa3b996
Bryan Krauss <bryan@button.is>
2022-05-29 13:47:02 -0700

    refactor: navbar now uses reactstrap

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,24 @@
 import Image from 'next/image';
+import { Nav, NavItem, NavLink } from 'reactstrap';
 
 export default function Navbar() {
   return (
-    <nav className="navbar bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          <Image
-            src="/public/vercel.svg"
-            alt="Chat app logo"
-            width="30"
-            height="24"
-            className="d-inline-block align-text-top"
-          ></Image>
+    <Nav className="bg-light">
+      <NavItem>
+        {' '}
+        <Image
+          src="/vercel.svg"
+          alt="Chat app logo"
+          width="60"
+          height="48"
+          className="d-inline-block align-text-top"
+        ></Image>
+      </NavItem>
+      <NavItem>
+        <NavLink active href="#">
           Welcome to Chat App
-        </a>
-      </div>
-    </nav>
+        </NavLink>
+      </NavItem>
+    </Nav>
   );
 }
