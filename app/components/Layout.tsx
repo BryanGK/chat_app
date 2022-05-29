cf4601f363befe43c186b9780a452a66249b5f6a
Bryan Krauss <bryan@button.is>
2022-05-29 13:48:22 -0700

    refactor: layout page extracting components

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,12 +1,44 @@
-import { Navbar, MessagesTable, UsersTable } from '../components';
+import { Navbar, MessagesTable, UsersTable, MessageInput } from '../components';
+import { Button, Input } from 'reactstrap';
+import { useState } from 'react';
 
-export default function Layout() {
+interface User {
+  id: string;
+  username: string;
+}
+
+interface Message {
+  id: string;
+  message: string;
+  author: string;
+}
+
+const Layout: React.FC = () => {
+  const [users, setUsers] = useState<User[]>([
+    { id: '1234', username: 'Bryan' },
+    { id: '2345', username: 'Andy' },
+  ]);
+  const [messages, setMessages] = useState<Message[]>([
+    { id: '1', message: 'hello world', author: 'Bryan' },
+    { id: '2', message: 'hey man!', author: 'Andy' },
+    { id: '3', message: 'doing great work!', author: 'Andy' },
+  ]);
+
   return (
     <div>
       <Navbar></Navbar>
-      <h1>Chat App</h1>
-      <UsersTable></UsersTable>
-      <MessagesTable></MessagesTable>
+      <div className="main">
+        <h1>Chat App</h1>
+        <div className="msg-main">
+          <div className="display">
+            <UsersTable users={users} />
+            <MessagesTable messages={messages} />
+          </div>
+          <MessageInput />
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Layout;
