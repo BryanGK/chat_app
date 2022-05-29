b175918a9284b2aeb5d05a0100e6332985fec760
Bryan Krauss <bryan@button.is>
2022-05-29 13:46:26 -0700

    feat: msg/user table displays

--- app/components/MessagesTable.tsx
+++ app/components/MessagesTable.tsx
@@ -1,3 +1,25 @@
-export default function MessagesTable() {
-  return <div></div>;
+interface Message {
+  id: string;
+  message: string;
+  author: string;
 }
+
+interface MessagesList {
+  messages: Message[];
+}
+
+const MessagesTable: React.FC<MessagesList> = ({ messages }) => {
+  return (
+    <div className="msg-table">
+      {messages.map((msg) => {
+        return (
+          <div key={msg.id}>
+            {msg.message} {msg.author}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default MessagesTable;
