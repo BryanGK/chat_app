ed508703b6dc5daf2eb0e95d6cc668f6e2b372a1
Bryan Krauss <bryan@button.is>
2022-06-19 08:48:48 -0700

    refactor: prettier format changes

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,7 +1,11 @@
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app';
-import { ClientToServerEvents, Navbar, ServerToClientEvents } from '../components';
+import {
+  ClientToServerEvents,
+  Navbar,
+  ServerToClientEvents,
+} from '../components';
 import { io, Socket } from 'socket.io-client';
 
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
@@ -10,7 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
       <Navbar></Navbar>
-      <Component {...pageProps} socket={socket}/>
+      <Component {...pageProps} socket={socket} />
     </div>
   );
 }
