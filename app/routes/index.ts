9d421e7b6cc242d5c6c4a2f5dc942c42835d7358
Bryan Krauss <bryan@button.is>
2022-06-21 11:58:56 -0700

    refactor: adds auth middleware to messages route

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -2,29 +2,37 @@ import express from 'express';
 import { Request, Response } from 'express';
 import { MessageEntity } from '../database/entity/MessageEntity';
 import { UserEntity } from '../database/entity/UserEntity';
+import authenticateToken from '../middleware/authenticateToken';
 import {
   getData,
   getUserById,
   postMessage,
   postUser,
+  login,
 } from '../services/databaseService';
 
 const apiRouter = express();
 
-apiRouter.get('/messages', async (req: Request, res: Response) => {
-  try {
-    const messages = await getData(MessageEntity);
-    res.send(messages);
-  } catch (error) {
-    res.send(`Error getting messages: ${error}`);
+apiRouter.get(
+  '/messages',
+  authenticateToken,
+  async (req: Request, res: Response) => {
+    try {
+      const messages = await getData(MessageEntity);
+      res.send(messages);
+    } catch (error) {
+      res.sendStatus(500).send(`Error getting messages: ${error}`);
+      res.send(`Error getting messages: ${error}`);
+    }
   }
-});
+);
 
 apiRouter.post('/messages', async (req: Request, res: Response) => {
   try {
     const message = await postMessage(req.body);
     res.send(message);
   } catch (error) {
+    res.sendStatus(500).send(`Error getting messages: ${error}`);
     console.error(`Error saving message: ${error}`);
   }
 });
@@ -34,7 +42,8 @@ apiRouter.get('/users', async (req: Request, res: Response) => {
     const users = await getData(UserEntity);
     res.send(users);
   } catch (error) {
-    res.send(`Error getting messages: ${error}`);
+    res.sendStatus(500).send(`Error getting users: ${error}`);
+    res.send(`Error getting users: ${error}`);
   }
 });
 
@@ -43,6 +52,7 @@ apiRouter.post('/users', async (req: Request, res: Response) => {
     const user = await postUser(req.body);
     res.send(user);
   } catch (error) {
+    res.sendStatus(500).send(`Error saving user: ${error}`);
     console.error(`Error saving user: ${error}`);
   }
 });
@@ -52,8 +62,20 @@ apiRouter.get('/users/:id', async (req: Request, res: Response) => {
     const user = await getUserById(parseInt(req.params.id));
     res.send(user);
   } catch (error) {
+    res.sendStatus(500).send(`Error saving user: ${error}`);
     console.error(`Error saving user: ${error}`);
   }
+});
+
+apiRouter.post('/login', (req: Request, res: Response) => {
+  login(req.body)
+    .then((accessToken) => {
+      res.send(JSON.stringify(accessToken));
+    })
+    .catch((error) => {
+      if (error.message.startsWith('User')) res.status(404).send(error.message);
+      else res.status(403).send(error.message);
+    });
 });
 
 export default apiRouter;
