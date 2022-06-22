66f6bda4ab326a407d8feec538c62f9c22cc06af
Bryan Krauss <bryan@button.is>
2022-06-22 10:25:51 -0700

    refactor: adds more data to user on auth

--- app/services/authService.ts
+++ app/services/authService.ts
@@ -3,14 +3,23 @@ import { User } from '../components';
 import { UserEntity } from '../database/entity/UserEntity';
 import jwt from 'jsonwebtoken';
 
-const authorizeUser = async (input: UserEntity, user: User) => {
+const authorizeUser = async (input: User, user: UserEntity) => {
   const isAuth = await bcrypt.compare(input.password, user.password);
   if (!isAuth) throw new Error('Incorrect password');
 
-  return jwt.sign(
+  const authToken = jwt.sign(
     JSON.stringify(user),
     process.env.ACCESS_TOKEN_SECRET as string
   );
+
+  const authorizedUser: User = {
+    id: user.id,
+    username: user.username,
+    password: '',
+    authToken: authToken,
+  };
+
+  return authorizedUser;
 };
 
 export default authorizeUser;
