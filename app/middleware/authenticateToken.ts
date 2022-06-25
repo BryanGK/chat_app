3b8a9ff3c05f9d46c9772778dbedb35f515ec25e
Bryan Krauss <bryan@button.is>
2022-06-25 12:08:11 -0700

    feat: get token from cookie in authToken

--- app/middleware/authenticateToken.ts
+++ app/middleware/authenticateToken.ts
@@ -2,6 +2,11 @@ import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 
 const authenticateToken = (req: Request, res: Response, next: Function) => {
+  const cookieHeader = req.headers['cookie'];
+  const regex = /(?<=access_token=)[^]+/;
+  if (!cookieHeader) return res.sendStatus(401);
+  const match = regex.exec(cookieHeader);
+  console.log('regex match', match![0]);
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader?.split(' ')[1];
   if (!token) return res.sendStatus(401);
